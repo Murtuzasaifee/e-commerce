ec8e7d39fcc8da301dcf696abb3f4bdae90e3747
Murtuza Saifee <>
2024-09-24 15:40:51 +0400

    Spring Cloud Feign and Wiremock
    Sync Communication established between Order and Inventory service using Spring Cloud Feign and test using Wiremock

--- order-service/src/main/java/com/ms/ecom/order/service/OrderService.java
+++ order-service/src/main/java/com/ms/ecom/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ms.ecom.order.service;
 
+import com.ms.ecom.order.client.InventoryClient;
 import com.ms.ecom.order.dto.OrderRequest;
 import com.ms.ecom.order.model.Order;
 import com.ms.ecom.order.repository.OrderRepository;
@@ -13,17 +14,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
+        //Check if item is in stock
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        //Map order request to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        if (isProductInStock) {
+            //Map order request to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
 
-        // save order to OrderRepository
-        orderRepository.save(order);
+            // save order to OrderRepository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product  with skuCode " + orderRequest.skuCode() + " is out of stock");
+        }
+
     }
 }
---
+++ order-service/src/test/java/com/ms/ecom/order/stubs/InventoryClientStub.java
@@ -0,0 +1,16 @@
+package com.ms.ecom.order.stubs;
+
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class InventoryClientStub {
+
+    public static void stubInventoryCall(String skuCode, Integer quantity){
+        stubFor(get(urlEqualTo("/api/inventory?skuCode="+skuCode+"&quantity="+quantity))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withStatus(200)
+                        .withBody("true")));
+    }
+}
