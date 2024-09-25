cf4377c626e207971957fbc8353222683d3ec8cd
Murtuza Saifee <>
2024-09-25 18:30:00 +0400

    KeyCloak implementation for authorizing the API with client id and secret

---
+++ api-gateway/src/main/java/com/ms/ecom/gateway/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ms.ecom.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+/*    // Inject the 'jwk-set-uri' from properties
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // Use the injected jwkSetUri
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return  httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
